18724a1cca0e039594a823e7c8206cf07f661d8f
Viviana <vivianaaarenas@gmail.com>
2021-01-06 22:33:13 -0500

    getting framework for the page down

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
-var APIKey = ""
-let searchButton = $("SearchButton");
+let searchButton = $("#SearchButton");
+var location = $("#searchInput").val().trim();
 
-$("#searchButton").on("click", function(event) {
-    alert("Works?")
-    event.preventDefault(); 
+var APIKey = "3de6b63c33a60d225104b1ff597cb08d";
+var queryURL =
+  "https://api.openweathermap.org/data/2.5/weather?" +
+  "q=" +
+  location +
+  "&appid=" +
+  APIKey;
 
-    var location = $("#searchInput").val();
-    console.log($("#searchInput").val())
+$("#searchButton").on("click", function (event) {
+  alert("Works?");
+  event.preventDefault();
+  console.log($("#searchInput").val());
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    //display response in the console log
+    console.log(reponse);
+    //   $(".city").html("<h1>" + response.name + "Weather</h1>");
+    //   $(".date").text(
+    //     luxon.DateTime.local().toLocaleString({
+    //       weekday: "long",
+    //       month: "long",
+    //       day: "2-digit",
+  });
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=" + location + "&appid=" + APIKey;
+  //   let weatherArt = response.weather[0].icon;
+  //   let iconurl = "https://openweatherman.org/img/w/" + weatherArt
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        //display response in the console log
-        console.log(reponse)
-        $(".city").html("<h1>" + response.name + "Weather</h1>");
-        $(".date").text(luxon.DateTime.local().toLocaleString({
-            weekday: "long",
-            month: "long",
-            day: "2-digit",
-        }));
+  //
 
-        let weatherArt = response.weater[0].icon;
-        let iconurl = "https://openweatherman.org/img/w/" + weatherArt 
-         
+  //   //
+  //   console.log($("#searchInput").val());
+  // var cardEl = $("<div>").addClass("card");
+  // // var imageEl = $("<img>");
+  // // imageEl.attr({ src: weather
+  // var locationInfo = location.info;
+  // var cardBodyEl = $("<div>").addClass("card-body");
+  // var cardTitleEl = $("<h5>").text(locationInfo)
+});
 
-    }
-}
+// api.openweathermap.org/data/2.5/forecast?q= + {city name},{state code},{country code}&appid={API key}
+
+// //   <div class="card" style="width: 18rem">
+//   <img class="card-img-top" src="..." alt="Card image cap" />
+//   <div class="card-body">
+//     <h5 class="card-title">Card title</h5>
+//     <p class="card-text">
+//       Some quick example text to build on the card title and make up the
+//       bulk of the card's content.
+//     </p>
+//     <a href="#" class="btn btn-primary">Go somewhere</a>
+
+//   //
+//);
